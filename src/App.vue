25ba73c7d79788acc761b4d997f6acda062aa3b2
ZackyKing <zackyargya@gmail.com>
2024-05-10 23:27:13 +0700

    adding audio back

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div id="app">
     <router-view></router-view>
   </div>
+  <audio id="bgm" autoplay loop>
+      <source src="../assets/bgm.mp3" type="audio/mp3">
+      Your browser does not support the audio element.
+    </audio>
 </template>
   
 <script setup>
